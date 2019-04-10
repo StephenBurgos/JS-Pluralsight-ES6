d2a0ed2b6e7f1a1cde97b564c89817d46dddff6b
Stephen Burgos <stephenburgos@Justins-MBP.SardiusMedia>
2019-04-10 09:54:58 -0500

    Finished New ES6 Syntax on Pluralsight

--- script.js
+++ script.js
@@ -16,8 +16,8 @@ function updatePar(newText = "Hello"){
     console.log(newText);
 }
 function clearPar(){
+    console.log(`Cleared "${textArea.innerText}" from the text area.`)
     textArea.innerText = "";
-    console.log("Paragraph cleared");
 }
 
 // PLURALSIGHT FUNCTIONS ////
@@ -26,12 +26,24 @@ function showCategories(productID, ...categories){
 }
 
 // VARIABLES ////////////////
+let Chocolate = {
+    good : "White Chocolate",
+    okay : "Dark Chocolate",
+    best : "Milk Chocolate"
+}
+let { good : whiteChoc, best : milkChoc, okay : darkChoc } = Chocolate;
 let prices = [12,20,18];
 let maxPrice = Math.max(...prices);
-
-
+let myForOfString = "Hello World!";
 // START ////////////////////
 pushButton.addEventListener('click', function(){
     showCategories(123, 'search', 'advertising');
     updatePar(maxPrice);
+    for (let letter of myForOfString){
+        updatePar(letter);
+    }
+    console.log(0o10); // Octal number
+    console.log(0b10); // Binary number
+    clearPar();
+    updatePar(`The best chocolate is ${milkChoc}, but ${whiteChoc} is also good.  And ${darkChoc} will do in a pinch.`);
 });
